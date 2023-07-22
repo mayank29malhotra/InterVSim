c411d43e43b00c39204111f2c2c95addbcd62edb
MAYANK MALHOTRA <95639908+mayank29malhotra@users.noreply.github.com>
2023-07-23 00:27:04 +0530

    Update RetrofitAPI.java
    Removed

--- InterviewBot/app/src/main/java/com/example/interviewbot/RetrofitAPI.java
+++ InterviewBot/app/src/main/java/com/example/interviewbot/RetrofitAPI.java
@@ -1,11 +1,1 @@
-package com.example.interviewbot;
 
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Url;
-public interface RetrofitAPI {
-
-    @GET
-    Call<MsgModel> getMessage(@Url String url);
-
-}
